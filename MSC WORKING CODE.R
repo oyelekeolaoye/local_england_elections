b8cee51ad98a793a364b09c398aa78106fdc29e7
oyelekeolaoye <oyelekeolaoye@gmail.com>
2019-06-18 08:31:21 -0500

    Working on merging the two data sources.....

---
+++ MSC WORKING CODE.R
@@ -0,0 +1,24 @@
+
+#trying to extract the result list
+capture.output(summary(results), file = "My New File.txt")
+write.table(results,"tes.txt",sep=";")
+cat(capture.output(print(results), file="test.txt"))
+
+# for loop for putting values in the brexit data frame
+for (i in 1:342) {
+  if frame$LAName == names(results)
+  frame$seats_available = sum()
+  
+}
+
+
+#exploratory analysis
+
+plot(brexit$Leave, brexit$)
+head(frame$LAName)
+head(names(results))
+
+#merging the two data sources
+sum(newdt$LAName==names(results))
+newdt$LAName==names(results)
+summary(results.df$elected)
