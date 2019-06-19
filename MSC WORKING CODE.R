514b7e006c9516dc392ecb60d957f51fb9e1a66f
oyelekeolaoye <oyelekeolaoye@gmail.com>
2019-06-19 17:50:38 -0500

    some exploratory analysis and trying to merge the two data sources

--- MSC WORKING CODE.R
+++ MSC WORKING CODE.R
@@ -1,27 +1,34 @@
+library(tidyr)
+library(ggplot2)
+library(dplyr)
 
-#trying to extract the result list
-capture.output(summary(results), file = "My New File.txt")
-write.table(results,"tes.txt",sep=";")
-cat(capture.output(print(results), file="test.txt"))
+#exploratory analysis for the brexit data frame
+head(brexit)
+tail(brexit)
+glimpse(brexit)
+summary(brexit)
+boxplot(brexit[,-c(1,2)]) ##all initial continous variable boxplot....no unusual values apart of outliers...all variables have been standardized prior to data exploration
+hist(brexit$AB)
+hist(brexit$C1)
+hist(brexit$C2)
+hist(brexit$DE)
+hist(brexit$ConVote)
+hist(brexit$LabVote)
+hist(brexit$LDVote)
+hist(brexit$UKIPVote)
+hist(brexit$GreenVote)
+hist(brexit$Leave) #all initial histogram of variables...most variables not-normal
 
-# for loop for putting values in the brexit data frame
-for (i in 1:342) {
-  if frame$LAName == names(results)
-  frame$seats_available = sum()
-  
-}
+#creating new columns to begin merge
+brexit$control_2019 <- NA 
+brexit$seats_available <- NA
+brexit$con_2019 <- NA
+brexit$lab_2019 <- NA
+brexit$ld_2019 <- NA
+brexit$ukip_2019 <- NA
+brexit$green_2019 <- NA
 
-
-#exploratory analysis
-
-plot(brexit$Leave, brexit$)
-head(frame$LAName)
-head(names(results))
-
-#merging the two data sources
-sum(newdt$LAName==names(results))
-newdt$LAName==names(results)
-summary(results.df$elected)
-
-colnames(new)[18] <- 'Control2019'
-sum(results$Allerdale$elected)
+#exploring the list (results)
+summary(results)
+results[[2]]
+str(results)
